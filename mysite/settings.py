8f52632c163d427578dd5a94890aaa935dc7e163
Lisandro <kelechul@gmail.com>
2016-07-04 15:54:07 -0300

    Secure the website

--- mysite/settings.py
+++ mysite/settings.py
@@ -98,6 +98,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Login
+
+LOGIN_REDIRECT_URL = '/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
