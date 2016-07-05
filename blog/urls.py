01b69bbb192b235dab630e5d27c66d8a3e6983d2
Lisandro <kelechul@gmail.com>
2016-07-05 17:43:59 -0300

    Create comment model

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<primary_key>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<primary_key>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^post/(?P<primary_key>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<primary_key>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<primary_key>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -51,7 +51,34 @@ def post_publish(request, primary_key):
     post.publish()
     return redirect('blog.views.post_detail', primary_key=primary_key)
 
+@login_required
 def post_remove(request, primary_key):
     post = get_object_or_404(Post, pk=primary_key)
     post.delete()
     return redirect('blog.views.post_list')
+
+def add_comment_to_post(request, primary_key):
+    post = get_object_or_404(Post, pk=primary_key)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blog.views.post_detail', primary_key=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, primary_key):
+    comment = get_object_or_404(Comment, pk=primary_key)
+    comment.approve()
+    return redirect('blog.views.post_detail', primary_key=comment.post.pk)
+
+@login_required
+def comment_remove(request, primary_key):
+    comment = get_object_or_404(Comment, pk=primary_key)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog.views.post_detail', primary_key=post_pk)
